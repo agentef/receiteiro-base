e02926234ecf015667265e445e9965f01d3921d5
Felipe Leite Uematsu <felipe.uematsu@digio.com.br>
2020-09-10 09:50:45 -0300

    Updated BaseException.kt

--- src/main/kotlin/br/com/receiteiro/base/exception/BaseException.kt
+++ src/main/kotlin/br/com/receiteiro/base/exception/BaseException.kt
@@ -1,6 +1,7 @@
 package br.com.receiteiro.base.exception
 
 import org.springframework.http.HttpStatus
+import org.springframework.web.client.HttpServerErrorException
 
 open class BaseException(var httpStatus: HttpStatus, var description: String) : RuntimeException() {
 
@@ -8,4 +9,6 @@ open class BaseException(var httpStatus: HttpStatus, var description: String) : 
     val codePrefix: String
         get() = _codePrefix
 
+    override val cause: Throwable?
+        get() = HttpServerErrorException(httpStatus)
 }
